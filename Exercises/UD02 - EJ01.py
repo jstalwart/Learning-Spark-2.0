16b035797c97f6aaeee82f6aabf7da2a79c4c813
Arnau Garcia <arnau.garcia@bosonit.com>
2023-02-02 11:52:28 +0100

    Exercise UD02-1 added

--- Exercises/UD02 - EJ01.py
+++ Exercises/UD02 - EJ01.py
@@ -22,20 +22,20 @@ quijote_string = (spark.read
                 .text(quijote_file))
 
 # El método count cuenta el número de filas del documento
-#print("Contador: ", quijote_string.count())
+print("Contador: ", quijote_string.count())
 
 # El método show muestra por la consola de comandos las n primeras líneas del documento
 # n es el número de líneas que muestra. Si no tiene valor devuelve las 20 primeras filas
 # truncate indica si se corta la línea o si se muestra entera
-#quijote_string.show(n = 2, truncate=False)
+quijote_string.show(n = 2, truncate=False)
 
 # El método head devuelve las n primeras filas
 # En texto devuelve una lista con las filas leídas 
-#print((quijote_string.head(10)))
+print((quijote_string.head(2)))
 
 # El método take hace lo mismo que el método head, pero almacenando los valores en una lista de filas, indicando pues todas
 # las variables de dicha fila. 
-#print(quijote_string.take(2))
+print(quijote_string.take(2))
 
 # El método first devuelve la primera fila del dataframe, indicanod todas las variables de dicha fila
 print(quijote_string.first())
