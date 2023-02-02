74f0021e4aab1f7ce12704a0b9e855d8b6588e30
Arnau Garcia <arnau.garcia@bosonit.com>
2023-02-02 11:38:18 +0100

    Exercise UD02-2 -> Show method

--- Exercises/UD02 - EJ01.py
+++ Exercises/UD02 - EJ01.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 import sys
 
 from pyspark.sql import SparkSession
@@ -16,8 +18,17 @@ spark = (SparkSession.builder
 # Recuperar el path del fichero
 quijote_file = sys.argv[1]
 
-quijote_df = (spark.read
+quijote_string = (spark.read
                 .text(quijote_file))
+
+print("Contador: ", quijote_string.count())
+
+# El método show muestra por la consola de comandos las n primeras líneas del documento
+# n es el número de líneas que muestra
+# truncate indica si se corta la línea o si se muestra entera
+quijote_string.show(n=2, truncate=False)
+
+
 
 spark.stop()
 
